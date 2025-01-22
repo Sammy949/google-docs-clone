1b8057e3fa7911592cfccd343688f72a838fb5cb
Sammy949 <urahsamuel0202@gmail.com>
2025-01-23 00:22:51 +0100

    Add documents schema and API query; integrate document fetching in Home component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import Navbar from "./navbar";
 import { TemplateGallery } from "./template-gallery";
+import { api } from "../../../convex/_generated/api";
+import { useQuery } from "convex/react";
 
 export default function Home() {
+  const documents = useQuery(api.documents.getDocuments);
+  if (documents === undefined) return <p>Documents are Loading...</p>;
+
   return (
     <div className="flex min-h-screen flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -9,6 +16,9 @@ export default function Home() {
       </div>
       <div className="mt-16">
         <TemplateGallery />
+        {documents?.map((document) => (
+          <span key={document._id}>{document.title}</span>
+        ))}
       </div>
     </div>
   );
