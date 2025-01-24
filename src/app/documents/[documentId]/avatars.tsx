287ab2f380bee36d0196c6261cadaf457acd9603
Sammy949 <urahsamuel0202@gmail.com>
2025-01-25 00:21:57 +0100

    Add inbox notifications feature; implement Inbox component and integrate with real-time notifications

--- src/app/documents/[documentId]/avatars.tsx
+++ src/app/documents/[documentId]/avatars.tsx
@@ -6,7 +6,6 @@ import {
   useOthers,
   useSelf,
 } from "@liveblocks/react/suspense";
-import Image from "next/image";
 
 const AVATAR_SIZE = 36;
 
@@ -24,7 +23,7 @@ const Avatar = ({ src, name }: AvatarProps) => {
       <div className="opacity-0 group-hover:opacity-100 absolute top-full py-1 px-2 text-white text-xs rounded-lg mt-2 5 z-10 bg-black whitespace nowrap transition-opacity">
         {name}
       </div>
-      <Image alt={name} src={src} className="size-full rounded-full" />
+      <img alt={name} src={src} className="size-full rounded-full" />
     </div>
   );
 };
--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -39,6 +39,7 @@ import { BsFilePdf } from "react-icons/bs";
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Avatars } from "./avatars";
+import { Inbox } from "./inbox";
 
 const Navbar = () => {
   const { editor } = useEditorStore();
@@ -265,6 +266,7 @@ const Navbar = () => {
       </div>
       <div className="flex gap-3 items-center pl-6">
         <Avatars />
+        <Inbox />
         <OrganizationSwitcher
           afterCreateOrganizationUrl="/"
           afterLeaveOrganizationUrl="/"
